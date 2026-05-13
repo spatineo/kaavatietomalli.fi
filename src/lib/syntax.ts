e1c2be86e7aa245adfdd68e74aeaa466f7c4dde4
Ilkka Rinne <ilkka.rinne@spatineo.com>
2026-05-13 15:38:11 +0300

    Another fix attempt on the syntax highlighter loading

--- src/lib/syntax.ts
+++ src/lib/syntax.ts
@@ -1,4 +1,4 @@
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter/dist/cjs/index';
 import vscDarkPlus from 'react-syntax-highlighter/dist/cjs/styles/prism/vsc-dark-plus';
 import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
 import ts from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
